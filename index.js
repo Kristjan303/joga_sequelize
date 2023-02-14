c2ff518132955bcf7568a20251beb04828f7b473
Kristjan-Even <kristjan.puusepp@voco.ee>
2023-02-14 14:34:16 +0200

    Created connection to database

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require ("express");
 const app = express ();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+const Sequelize = require ("sequelize");
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize')
+
+sequelize
+    .authenticate()
+    .then(() =>{
+        console.log('CONNNECTION HAS BEEN MADE WITH the DATA BASE!!!!!');
+    })
+    .catch(err => {
+        console.error('Unableness TO BE CONNECTED has been CONFIRMED BY THE DATABASE', err)
+    })
+
 app.get("/", (req, res) => {
     res.json({ message: "welcome to sequel - ižžžžžžerrrrrr app."})
 })
